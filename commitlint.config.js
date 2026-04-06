a8ce9fb0e0a9d93f486fa0edf1e7d9b7c4dfdaa5
AHuyHarryIT <anhhuyharryxit@gmail.com>
2026-04-07 04:04:00 +0700

    fix(commitlint): increase body max line length to 200 characters

--- commitlint.config.js
+++ commitlint.config.js
@@ -26,7 +26,7 @@ export default {
     'subject-full-stop': [2, 'never', '.'],
     'header-max-length': [2, 'always', 100],
     'body-leading-blank': [1, 'always'],
-    'body-max-line-length': [2, 'always', 100],
+    'body-max-line-length': [2, 'always', 200],
     'footer-leading-blank': [1, 'always'],
     'footer-max-line-length': [2, 'always', 100]
   }
